7be09a3e9047d1eb37a30dfbbc4de87d49b2f2bb
Jörg Hedtmann <joerg.hedtmann@geomarineconsult.com>
2021-12-15 03:41:35 +0100

    Moved existing code into repo.

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'iobroker-vbbadapter adapter settings': {
+        'en': 'Adapter settings for iobroker-vbbadapter',
+        'de': 'Adaptereinstellungen für iobroker-vbbadapter',
+        'ru': 'Настройки адаптера для iobroker-vbbadapter',
+        'pt': 'Configurações do adaptador para iobroker-vbbadapter',
+        'nl': 'Adapterinstellingen voor iobroker-vbbadapter',
+        'fr': "Paramètres d'adaptateur pour iobroker-vbbadapter",
+        'it': "Impostazioni dell'adattatore per iobroker-vbbadapter",
+        'es': 'Ajustes del adaptador para iobroker-vbbadapter',
+        'pl': 'Ustawienia adaptera dla iobroker-vbbadapter',
+        'zh-cn': 'iobroker-vbbadapter的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
